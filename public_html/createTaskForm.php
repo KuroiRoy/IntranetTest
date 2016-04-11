0ab0625df47eafe6ab4c16cb6f9a6eb7e17637e1
KuroiRoy <roy.zwart@gmail.com>
2016-04-11 11:00:32 +0200

    Adding link between user and task when creating a task

--- public_html/createTaskForm.php
+++ public_html/createTaskForm.php
@@ -11,12 +11,20 @@ $taskname = $_POST['taskname'];
 $description = $_POST['description'];
 
 $currentUserQuery = DB::queryFirstRow('SELECT * FROM users WHERE username = %s', $_SESSION['username']);
+$userID = $currentUserQuery['id'];
 
 DB::insert('tasks', array(
 	'name' => $taskname,
 	'description' => $description,
 	'is_completed' => false,
 	'creator_id' => $currentUserQuery['id']
+));
+
+$taskID = DB::insertId();
+
+DB::insert('link_user_task', array(
+	'user_id' => $userID,
+	'task_id' => $taskID
 ));
 
 header('Location: tasks.php');
